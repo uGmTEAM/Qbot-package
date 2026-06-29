7720f4a15691a3624c26fe877fd4682c047ca3ee
uGmTEAM <undermountain01@hotmail.com>
2026-06-29 19:08:05 +0800

    Add files via upload

---
+++ Install_Qbot.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+apt update -y
+apt install curl pipx python3-pip -y
+curl -L -o uv.tar.gz https://proxy.gitwarp.top/https://github.com/astral-sh/uv/releases/download/0.11.25/uv-x86_64-unknown-linux-gnu.tar.gz
+tar -xzf uv.tar.gz
+mv uv /usr/local/bin/
+chmod +x /usr/local/bin/uv
+curl -LsSf https://docs.astrbot.app/install.sh | bash
+export PATH="/root/.local/bin:$PATH"
+echo 'export PATH="/root/.local/bin:$PATH"' >> /root/.bashrc
+astrbot init -y
+curl -o napcat.sh https://raw.githubusercontent.com/NapNeko/napcat-linux-installer/refs/heads/main/install.sh && bash napcat.sh
+echo 'bot安装已完成，请执行 "astrbot run" 和 "./launcher" 并在程序界面打开QQ以运行，脚本整合：umMAKER，Bilibili：space.bilibili.com/1143250330。感谢使用！'
