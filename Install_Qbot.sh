f51023534e2d614004b02404045fbf9c98c74993
uGmTEAM <undermountain01@hotmail.com>
2026-06-29 20:58:24 +0800

    Update Install_Qbot.sh

--- Install_Qbot.sh
+++ Install_Qbot.sh
@@ -2,8 +2,8 @@
 apt update -y
 apt install curl pipx python3-pip -y
 curl -L -o uv.tar.gz https://proxy.gitwarp.top/https://github.com/astral-sh/uv/releases/download/0.11.25/uv-x86_64-unknown-linux-gnu.tar.gz
-tar -xzf uv-x86_64-unknown-linux-gnu.tar.gz
-cd　uv-x86_64-unknown-linux-gnu
+tar -xzf uv.tar.gz
+cd　uv
 mv uv /usr/local/bin/
 chmod +x /usr/local/bin/uv
 cd ..
