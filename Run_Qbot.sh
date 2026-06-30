422e1da3d4bddca84d6b5fc1c2bd5af2f9c70684
uGmTEAM <undermountain01@hotmail.com>
2026-06-30 17:12:34 +0800

    Update Run_Qbot.sh

--- Run_Qbot.sh
+++ Run_Qbot.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 gnome-terminal -- bash -c "./launcher.sh; exec bash"
-gnome-terminal -- bash -c "astrbot run"
-gnome-terminal -- bash -c "oloama serve"
+gnome-terminal -- bash -c "astrbot run; exec bash"
+gnome-terminal -- bash -c "oloama serve; exec bash"
 exit
 cd /opt/QQ
 ./qq
