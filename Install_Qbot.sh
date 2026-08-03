73f666b06f5de8b749562f015fcff38bd7771e0d
uGmTEAM <undermountain01@hotmail.com>
2026-08-03 21:23:48 +0800

    Update Install_Qbot.sh

--- Install_Qbot.sh
+++ Install_Qbot.sh
@@ -20,6 +20,6 @@ echo 'export PATH="/root/.local/bin:$PATH"' >> /root/.bashrc
 astrbot init -y
 curl -o napcat.sh https://raw.githubusercontent.com/NapNeko/napcat-linux-installer/refs/heads/main/install.sh && bash napcat.sh
 gnome-terminal -- bash -c "gtk-launch qq" 
-echo 'bot安装已完成，请在的Terminal中以root权限（先输入“sudo su”然后回车）执行 "bash ./Run_Qbot.sh" ，脚本整合：umMAKER，Bilibili：space.bilibili.com/1143250330。感谢使用！'
-sleep 10
+echo 'bot安装已完成，系统将会在20秒后重启，请在的Terminal中以root权限（先输入“sudo su”然后回车）执行 "bash ./Run_Qbot.sh" ，脚本整合：umMAKER，Bilibili：space.bilibili.com/1143250330。感谢使用！'
+sleep 20
 reboot
